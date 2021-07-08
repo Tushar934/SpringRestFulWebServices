60eb78fb56570846cf87ac59412bf03b0411b040
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-07-08 17:50:19 +0530

    Demo Rest API and Swagger
    Initial Commit

---
+++ Demo-Project-RestAPI- Swagger/src/main/java/com/cognizant/project/app/RestFulServicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulServicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulServicesDemoApplication.class, args);
+	}
+
+}
---
+++ Demo-Project-RestAPI- Swagger/src/test/java/com/cognizant/project/app/RestFulServicesDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cognizant.project.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulServicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
